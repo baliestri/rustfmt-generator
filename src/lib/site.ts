30ce38ab1beb949375773fc4e444d34097b707d6
Bruno Sales <me@baliestri.dev>
2026-09-18 23:25:09 -0300

    chore: license under mit

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -1,3 +1,4 @@
 export const REPO_URL = 'https://github.com/baliestri/rustfmt-generator'
 export const SITE_URL = 'https://baliestri.github.io/rustfmt-generator/'
 export const COPYRIGHT_HOLDER = 'Bruno Sales'
+export const LICENSE_URL = `${REPO_URL}/blob/main/LICENSE.md`
